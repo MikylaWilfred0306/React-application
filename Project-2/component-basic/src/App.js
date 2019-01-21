fb4ded7001682da0b42d70dbb59e23083d0ff9a6
MikylaWilfred0306 <mikyla.wilfred@gmail.com>
2019-01-21 10:59:52 -0500

    Constructors and Functions

--- Project-2/component-basic/src/App.js
+++ Project-2/component-basic/src/App.js
@@ -3,23 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props); 
+    this.yourname = "Brad";
+    this.state = {}; 
+  }
+
+  helloFunction(name){
+    return " and " + name;
+  }
+
   render() {
+    const myName = "Mikyla";
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     <h2>Hello World {this.helloFunction(myName)} {this.helloFunction(this.yourname)}</h2>
       </div>
     );
   }
