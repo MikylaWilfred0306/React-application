5812bb0feba0ae138d5b6096519f8f2b11fc0cb5
MikylaWilfred0306 <mikyla.wilfred@gmail.com>
2019-01-21 09:54:23 -0500

    Removed the original codes to then create my own
    create-react-app component-basic

--- Project-1/hello-world/src/App.js
+++ Project-1/hello-world/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
@@ -13,35 +14,5 @@ class App extends Component {
 }
 
 
-class Header extends Component {
-  render(){
-    return(
-      <div> 
- <header className="App-header">
-          <img src="https://msupodcastcom.files.wordpress.com/2018/08/main-logo-2.png?w=700&h=&crop=1" alt="logo" />
-          <p>
-           Come listen to my podcast.
-          </p>
-          <a
-            className="App-link"
-            href="https://msu-podcast.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Modern Shakespeare Uncensored
-          </a>
-          <a
-            className="App-link"
-            href="https://open.spotify.com/show/5ED4NKsDEgtBIKlzh8HcrS?si=ZFEA84KYTzGpIXJ0foGDQg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Spotify
-          </a>
-        </header>
-      </div>
-      );
-    }
-  }
 
 export default App;
--- Project-1/hello-world/src/Header.js
+++ Project-1/hello-world/src/Header.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import './App.css';
 
 
- class Header extends Component {
+class Header extends Component {
   render(){
     return(
-      <div>
-        <div className="App-header">
-          {
-          //<img src={logo} className="App-logo" alt="logo" />
-          }
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
-    );
-  }
-}
+      <div> 
+ <header className="App-header">
 
-export default Header;
+ {
+   //This is how to comment
+ }
+          <img src="https://msupodcastcom.files.wordpress.com/2018/08/main-logo-2.png?w=700&h=&crop=1" alt="logo" />
+          <p>
+           Come listen to my podcast.
+          </p>
+          <a
+            className="App-link"
+            href="https://msu-podcast.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Modern Shakespeare Uncensored
+          </a>
+          <a
+            className="App-link"
+            href="https://open.spotify.com/show/5ED4NKsDEgtBIKlzh8HcrS?si=ZFEA84KYTzGpIXJ0foGDQg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Spotify
+          </a>
+        </header>
+      </div>
+      );
+    }
+  }
+
+  export default Header;
